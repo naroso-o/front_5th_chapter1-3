6d0536a35e87b7db07f73e41e1f4a5c8632321fe
naroso_o <naroso1751@naver.com>
2025-04-06 17:30:38 +0900

    feat: equals functions

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,30 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
+  if (!objA || !objB) {
+    return objA === objB ? true : false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+
+    for (let i = 0; i < objA.length; i++) {
+      if (typeof objA[i] == "object" || typeof objB[i] == "object") {
+        return deepEquals(objA[i], objB[i]);
+      }
+      if (objA[i] !== objB[i]) return false;
+    }
+
+    return true;
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    for (const key in objA) {
+      if (typeof objA[key] == "object" || typeof objB[key] == "object") {
+        return deepEquals(objA[key], objB[key]);
+      }
+      if (objA[key] !== objB[key]) return false;
+    }
+
+    return true;
+  }
   return objA === objB;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,24 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (!objA || !objB) {
+    return objA === objB ? true : false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+
+    for (let i = 0; i < objA.length; i++) {
+      if (objA[i] !== objB[i]) return false;
+    }
+
+    return true;
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    for (const key in objA) {
+      if (objA[key] !== objB[key]) return false;
+    }
+
+    return true;
+  }
   return objA === objB;
 }
